462ba90cf88ce5e7b34314fe81b06cad7628ab97
Mishkat6541 <[mishkat6541@hotmail.co.uk]>
2024-09-14 20:47:40 +0100

    finish delete button functionality

--- vite-project/src/toDoList.jsx
+++ vite-project/src/toDoList.jsx
@@ -19,7 +19,10 @@ function ToDoList() {
         
     }
 
-    function deleteTask(index) {}
+    function deleteTask(index) {
+        const updatedTasks = tasks.filter((_,i) => (i != index));
+        setTasks(updatedTasks);
+    }
 
     function moveTaskUp(index) {}
 
